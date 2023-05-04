bcd478e5b8c81b327ee93330c626adfbc63a9471
omnunum <3474285+omnunum@users.noreply.github.com>
2023-05-03 22:19:59 -0400

    make animations loop, upscale for faster renders

--- numpy_generate_blobs.py
+++ numpy_generate_blobs.py
@@ -5,13 +5,11 @@ import numpy as np
 import cv2
 import noise
 import webcolors
-
-from line_profiler import LineProfiler
+from scipy.interpolate import RectBivariateSpline
 
 Color = list[int, int, int]
 WeightedColor = list[Color, float]
 
-@profile
 def process_colors(colors: str) -> Tuple[np.ndarray, np.ndarray]:
     """
     Parses a string representation of colors and weights and returns
@@ -40,7 +38,6 @@ def process_colors(colors: str) -> Tuple[np.ndarray, np.ndarray]:
     return color_array, cumulative_weights
 
 
-@profile
 def noise_to_color_array(noise: np.ndarray, color_array: np.ndarray, cumulative_weights: np.ndarray) -> np.ndarray:
     """
     Maps the noise values to colors based on the provided color_array and cumulative_weights.
@@ -78,31 +75,47 @@ def rescaled_noise(x, y, scale):
 @click.option("--height", default=480, type=int, help="Video height")
 @click.option("--fps", default=60, type=int, help="Frames per second")
 @click.option("--duration", default=10, type=int, help="Video duration in seconds")
-@click.option("--noise_scale", default=0.0033, type=float, help="Perlin noise scale")
-@click.option("--colors", default="#E3D2B5:2,#F2B950:5,#D99748:3,#D99AB7:5,#D989/url89:2", type=str, help="JSON list of CSS3 color names and weights")
-@profile
-def main(width: int, height: int, fps: int, duration: int, noise_scale: float, colors: str) -> None:
+@click.option("--noise_size_scale", default=0.0016, type=float, help="Scaling factor to determine the size (freqency) of the noise")
+@click.option("--noise_time_scale", default=0.1, type=float, help="Scaling factor to determine how quickly we move through the noise space")
+@click.option("--colors", default="#E3D2B5:2,#F2B950:5,#D99748:3,#D99AB7:5,#D98989:2", type=str, help="JSON list of CSS3 color names and weights")
+@click.option("--scale-factor", default=1, type=int, help="Factor to scale the output by")
+def main(width: int, height: int, fps: int, duration: int, noise_size_scale: float, noise_time_scale: float, colors: str, scale_factor: int) -> None:
     max_frames = fps * duration
     color_array, cumulative_weights = process_colors(colors)
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_writer = cv2.VideoWriter('perlin_noise_color_space.mp4', fourcc, fps, (width, height), True)
 
-    x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
+    x_coords, y_coords = np.meshgrid(np.arange(width // scale_factor), np.arange(height // scale_factor))
     vectorized_noise = np.vectorize(rescaled_noise)
 
     for t in range(max_frames):
         print(t)
+        small_frame = np.zeros((height // scale_factor, width // scale_factor, 3), dtype=np.uint8)
 
-        frame = np.zeros((height, width, 3), dtype=np.uint8)
-        noisey_t_x = t + vectorized_noise(t, t, noise_scale * 10)
-        noisey_t_y = t + vectorized_noise(t*2, t*2, noise_scale * 10)
-        space_noise_1 = vectorized_noise(x_coords + noisey_t_x, y_coords + noisey_t_y, noise_scale)
-        space_noise_2 = vectorized_noise(x_coords - noisey_t_x, y_coords - noisey_t_y, noise_scale)
+        # Calculate noisey_t_x and noisey_t_y for circular traversal
+        angle = (2 * np.pi * t * noise_time_scale) / max_frames
+        tx, ty = width * np.sin(angle), height * np.cos(angle)
+        noisey_t_x = tx + vectorized_noise(tx, tx, noise_size_scale)
+        noisey_t_y = ty + vectorized_noise(ty*2, ty*2, noise_size_scale)
+
+        space_noise_1 = vectorized_noise(x_coords + noisey_t_x, y_coords + noisey_t_y, noise_size_scale)
+        space_noise_2 = vectorized_noise(x_coords - noisey_t_x, y_coords - noisey_t_y, noise_size_scale)
 
         combined_noise = (space_noise_1 + space_noise_2 ) % 1
 
-        frame = noise_to_color_array(combined_noise, color_array, cumulative_weights) 
+        small_frame = noise_to_color_array(combined_noise, color_array, cumulative_weights)
+        
+        # Upscale the small frame using linear interpolation
+        x = np.arange(width // scale_factor)
+        y = np.arange(height // scale_factor)
+        x_new = np.linspace(0, width // scale_factor - 1, width)
+        y_new = np.linspace(0, height // scale_factor - 1, height)
+        frame = np.zeros((height, width, 3), dtype=np.uint8)
+        for i in range(3):
+            interpolator = RectBivariateSpline(y, x, small_frame[..., i], kx=1, ky=1)
+            frame[..., i] = interpolator(y_new, x_new)
+        
         frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert the frame from RGB
         video_writer.write(frame_bgr)
     video_writer.release()
