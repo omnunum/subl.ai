6d42f8d55334ac05b3f46d1abdddfaad9450c15f
omnunum <3474285+omnunum@users.noreply.github.com>
2023-05-16 23:05:14 -0400

    remove demo ts operations

--- .wundergraph/operations/users/get.ts
+++
@@ -1,8 +0,0 @@
-import { createOperation, z } from '../../generated/wundergraph.factory';
-
-export default createOperation.query({
-	handler: async ({ internalClient }) => {
-		const users = await internalClient.queries.Users();
-		return { ...users.data?.db_findFirstusers };
-	},
-});
